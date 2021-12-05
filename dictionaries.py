e425c74e18a4275d7f535c4870f68adceec25bab
lhjwork <jin.come.up.business@gmail.com>
2021-12-05 13:49:50 +0900

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,18 @@
+#dictionary는 data Type이다. 
+my_dict  ={
+  # name : key
+  # Tim : value
+  'name': 'Tim',
+  # 'name': 'jin',
+  'nationality': 'African',
+  'Qualification': 'College',
+  'age': 87,
+  'friends' : ['Peter', 'Paul', 'Preciout']
+}
+
+X = my_dict['name']
+
+print(len(my_dict))
+print(my_dict['age'])
+print(type(my_dict))
+
