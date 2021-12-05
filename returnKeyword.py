26d9afb0d561d363204f950a97b65f3cb26775fe
lhjwork <jin.come.up.business@gmail.com>
2021-12-05 13:39:02 +0900

    if statement and Number check Program

---
+++ returnKeyword.py
@@ -0,0 +1,10 @@
+def my_function():
+  return 5+4
+
+def add_numbers(num1, num2):
+  return num1+num2
+
+num1 = input('num1')
+num2 = input('num2')
+print(my_function())
+print(add_numbers(num1,num2))
---
+++ statements.py
@@ -0,0 +1,32 @@
+a = False
+b = 3
+
+if a == b:
+    print('a is equals to b')
+elif a == False:
+  print('A is false')
+else:
+    print('A is none of the two')
+
+boy = True
+short = True
+6
+
+if boy == False or short == False:
+  print('He is a boy or he is short')
+elif boy == False:
+  print('A is false')
+else :
+  print('A is none of the two')
+
+  value = int(input('Input a value : '))
+  
+  # if type(value) == str:
+  #   print(value + ' is a string')
+  # else:
+  #   print(value , ' is not a string')
+
+  if value%5 == 0:
+    print(value, 'can be divided by 5')
+  else:
+    print(value, 'can not be divided by 5 ')
